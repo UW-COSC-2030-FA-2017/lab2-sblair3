b33b0a02e3f669765b5a638ad287d3c88918d250
sblair3 <31827076+sblair3@users.noreply.github.com>
2017-09-18 18:49:32 -0600

    Update ListProcessor.cpp

--- code/ListProcessor.cpp
+++ code/ListProcessor.cpp
@@ -1,3 +1,4 @@
+//Samuel Blair
 // ListProcessor.cpp
 
 // tom bailey   1540  30 sep 2011
@@ -6,7 +7,7 @@
 // Exercise the List class with verbose Node constructor
 //   and destructor.
 
-#include "LinkedList.cpp"
+#include "LinkedList.h"
 
 
 int main()
@@ -16,35 +17,68 @@ int main()
 
 	cows.insertAsFirst(1.23);
 	cows.insertAsFirst(2.34);
-	cout << "cows list  :  " << cows << endl << endl;
+	cows.insertAsLast(4.12);
+	cout << "cows list  :  " << cows << endl ;
+	cout << "The sum is " << cows.listSum() << endl;
+	cout << "The size is " << cows.listSize() << endl<< endl;
 
 	cows.removeFirst();
-	cout << "cows list  :  " << cows << endl << endl;
+	cout << "cows list  :  " << cows << endl;
+	cout << "The sum is " << cows.listSum() << endl;
+	cout << "The size is " << cows.listSize() << endl << endl;
 
 	List horses(cows);
 	cout << "cows list  :  " << cows << endl;
-	cout << "horses list:  " << horses << endl << endl;
+	cout << "The sum is " << cows.listSum() << endl;
+	cout << "The size is " << cows.listSize() << endl << endl;
+	cout << "horses list:  " << horses << endl ;
+	cout << "The sum is " << horses.listSum() << endl;
+	cout << "The size is " << horses.listSize() << endl << endl;
 
 	horses.removeFirst();
 	horses.insertAsFirst(5.67);
+	horses.insertAsLast(9.32);
 	cows.insertAsFirst(6.78);
-	cout << "cows list  :  " << cows << endl;
-	cout << "horses list:  " << horses << endl << endl;
+	cout << "cows list  :  " << cows << endl; 
+	cout << "The sum is " << cows.listSum() << endl;
+	cout << "The size is " << cows.listSize() << endl << endl;
+	cout << "horses list:  " << horses << endl ;
+	cout << "The sum is " << horses.listSum() << endl;
+	cout << "The size is " << horses.listSize() << endl << endl;
 
 	List pigs;
+	pigs.insertAsLast(1.23);
 	cout << "cows list  :  " << cows << endl;
+	cout << "The sum is " << cows.listSum() << endl;
+	cout << "The size is " << cows.listSize() << endl << endl;
 	cout << "horses list:  " << horses << endl;
-	cout << "pigs list  :  " << pigs << endl << endl;
+	cout << "The sum is " << horses.listSum() << endl;
+	cout << "The size is " << horses.listSize() << endl << endl;
+	cout << "pigs list  :  " << pigs << endl ;
+	cout << "The sum is " << pigs.listSum() << endl;
+	cout << "The size is " << pigs.listSize() << endl << endl;
 
 	pigs = cows;
 	cout << "cows list  :  " << cows << endl;
+	cout << "The sum is " << cows.listSum() << endl;
+	cout << "The size is " << cows.listSize() << endl << endl;
 	cout << "horses list:  " << horses << endl;
-	cout << "pigs list  :  " << pigs << endl << endl;
+	cout << "The sum is " << horses.listSum() << endl;
+	cout << "The size is " << horses.listSize() << endl << endl;
+	cout << "pigs list  :  " << pigs << endl ;
+	cout << "The sum is " << pigs.listSum() << endl;
+	cout << "The size is " << pigs.listSize() << endl << endl;
 
 	pigs = horses;
 	cout << "cows list  :  " << cows << endl;
+	cout << "The sum is " << cows.listSum() << endl;
+	cout << "The size is " << cows.listSize() << endl << endl;
 	cout << "horses list:  " << horses << endl;
-	cout << "pigs list  :  " << pigs << endl << endl;
+	cout << "The sum is " << horses.listSum() << endl;
+	cout << "The size is " << horses.listSize() << endl << endl;
+	cout << "pigs list  :  " << pigs << endl ;
+	cout << "The sum is " << pigs.listSum() << endl;
+	cout << "The size is " << pigs.listSize() << endl << endl;
 
 	cout << "End of code" << endl;
 
